2936c027264a8144fd5929ec9a577a008341928b
birna14 <birna14@ru.is>
2019-10-26 23:56:29 +0000

    Needs page with radio buttons

--- lib/pages/needs.dart
+++ lib/pages/needs.dart
@@ -1,20 +1,127 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+
 class NeedsPage extends StatefulWidget {
-  @override
-  _NeedsPage createState() => _NeedsPage();
+   @override
+  State<StatefulWidget> createState() {
+    return new _NeedsPage();
+  }
+  
+ /* @override
+  _NeedsPage createState() => _NeedsPage();*/
 }
 
 class _NeedsPage extends State<NeedsPage> {
   final _formKey = GlobalKey<FormState>();
   String user;
-  Map<String, bool> needs = new Map<String, bool>();
-  TextEditingController _controller = TextEditingController();
+  Map<dynamic, dynamic> needs = new Map<String, String>();
+  int priority = 3;
 
-  void addNeed(String value) {
+  
+  void addNeed(String value, int priority) async {
+    //debugPrint(priority.toString());
+    // todo add to firestore
     if (value != '') {
-      setState(() => needs[value] = false);
+      var tmpPri = priority.toString();
+      try {
+      var jsonData = '{ "reporter": "Me", "priority": $tmpPri, "doer": "Stefán" }';
+      var parsedJson = json.decode(jsonData);
+      needs[value] = parsedJson;
+      } catch(e) {
+        print("error: $e");
+      }
+      
     }
+  }
+
+  int _radioValue = 0;
+  int value = 0;
+  
+  
+  _displayDialog(BuildContext context) async {
+      return showDialog(
+          context: context,
+          builder: (context) {
+            return StatefulBuilder(
+              builder: (context, setState) {
+                return new AlertDialog(
+                  title: Text("Make new task"),
+                  content:
+                    Center(
+                    child: Form(
+                      key: _formKey,
+                      child: Column( 
+                      mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[                            
+                          Padding(
+                              padding: EdgeInsets.all(8.0),
+                                  child: TextFormField(
+                                    onSaved: (String v) {
+                                      addNeed(v, priority);
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget> [
+                              new Radio(
+                                value: 1,
+                                groupValue: _radioValue,
+                                onChanged: (val) {
+                                  setState(() {
+                                    _radioValue = val;
+                                    priority = val;
+                                  });
+                                },
+                              ),
+                              new Text('High'),
+                              new Radio(
+                                value: 2,
+                                groupValue: _radioValue,
+                                onChanged: (val) {
+                                  setState(() {
+                                    _radioValue = val;
+                                    priority = val;
+                                  });
+                                },
+                              ),
+                              new Text('Medium'),
+                              new Radio(
+                                value: 3,
+                                groupValue: _radioValue,
+                                onChanged: (val) {
+                                  setState(() {
+                                    _radioValue = val;
+                                    priority = val;
+                                  });
+                                },
+                              ),
+                              new Text('Low')
+                            ]
+                          ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text("Cancel"),
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              _formKey.currentState.save();
+                            },
+                            child: Text("Add"),
+                          ),
+                    ]
+                  );
+                });
+              },
+            );
   }
 
   Widget build(BuildContext context) {
@@ -32,10 +139,14 @@ class _NeedsPage extends State<NeedsPage> {
               return new  Card(
                   child: ListTile(
                     leading: new Checkbox(
-                      value: needs[value],
+                      value: needs['doer'] == '' ? false : true,
                       onChanged: (v) {
                         setState(() {
-                          needs[value] = !needs[value];
+                          if (needs['doer'] == '') {
+                            needs['doer'] = 'Me';
+                          } else {
+                            needs['doer'] = '';
+                          }
                         });
                       },
                       ),
@@ -48,51 +159,10 @@ class _NeedsPage extends State<NeedsPage> {
         ]
         ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return StatefulBuilder(
-                  builder: (context, setState) {
-                    return AlertDialog(
-                      title: Text("Make new task"),
-                      content: Form(
-                        key: _formKey,
-                        child: Column( 
-                        mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                onSaved: (String value) {
-                                  addNeed(value);
-                                  Navigator.pop(context);
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: Text("Cancel"),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            _formKey.currentState.save();
-                          },
-                          child: Text("Add"),
-                        ),
-                      ]
-                    );
-                  },
-                );
-              }
-            );
-          },
-        ), 
-    );
-  }
+            child: Icon(Icons.add),
+            onPressed: () => _displayDialog(context)
+        )
+      );
+    }
+     
 }
